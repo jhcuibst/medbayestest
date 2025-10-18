d23b66e03ee915f7f3aa570ae63d44f8be2da0e8
Jinhong Cui <cuijinhongqk@gmail.com>
2025-10-17 20:48:55 -0400

    all func

---
+++ R/run _function.R
@@ -0,0 +1,35 @@
+
+#'
+#' @name medbayes
+#' @title Run Bayesian mediation model
+#'
+#' @param model.m A brms model for the mediator
+#' @param model.y A brms model for the outcome
+#' @param dat.new New data for prediction
+#' @return Posterior predictions (first column)
+#' @export
+medbayes <- function(model.m = model.m,
+                     model.y = model.y,
+                     treat = "treatment",
+                     mediator = "mediator",
+                     ind_mediator = NULL,
+                     outcome = "outcome",
+                     control.value = 0,
+                     treat.value = 1){
+
+  zi.mediator = grepl("zero", family(model.m)$family) | grepl("hurdle", family(model.m)$family)
+  zi.outcome = grepl("zero", family(model.y)$family) | grepl("hurdle", family(model.y)$family)
+
+  if(zi.outcome){
+    result <- medbayes_zim(model.m=model.m, model.y=model.y, treat=treat,
+                           mediator = mediator, ind_mediator = ind_mediator,
+                           outcome = outcome,
+                           control.value = control.value, treat.value = treat.value)
+  }else{
+    result <- medbayes_ziy(model.m=model.m, model.y=model.y, treat=treat,
+                           mediator = mediator, ind_mediator = ind_mediator,
+                           outcome = outcome,
+                           control.value = control.value, treat.value = treat.value)
+  }
+  return(result)
+}
